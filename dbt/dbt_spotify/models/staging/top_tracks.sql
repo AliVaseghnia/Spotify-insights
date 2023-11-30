197f2e79d212534d5e7a4ddd289e7ced090eef2c
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2023-11-29 21:03:31 -0500

    add more fields to top_tracks model

--- dbt/dbt_spotify/models/staging/top_tracks.sql
+++ dbt/dbt_spotify/models/staging/top_tracks.sql
@@ -1,8 +1,11 @@
 WITH source_data AS (
     SELECT
         tt.id,
+        JSON_VALUE(tt.json_column, '$.id') AS track_id,
         JSON_VALUE(tt.json_column, '$.name') AS track_name,
-        JSON_VALUE(tt.json_column, '$.popularity') AS popularity,
+        JSON_VALUE(tt.json_column, '$.duration_ms') AS track_duration,
+        JSON_VALUE(tt.json_column, '$.explicit') AS track_is_explicit,
+        JSON_VALUE(tt.json_column, '$.popularity') AS track_popularity,
         a.id AS artist_id,
         a.name AS artist_name
     FROM dbo.top_tracks tt
